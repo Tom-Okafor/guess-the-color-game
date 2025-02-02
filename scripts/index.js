f6c4faefacf5e428d42e49727ec45a366068c721
Tom Okafor <tmeka008@gmail.com>
2025-02-02 23:31:18 +0100

    update javascript file with functions needed to shuffle an array of random colours.

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,37 @@ const COLOURS = [
   "#4169e1",
   "#1e90ff",
 ];
+
+function generateRandomArrayIndex(array = COLOURS) {
+  const randomArrayIndex = Math.floor(Math.random() * array.length);
+  return randomArrayIndex;
+}
+
+function selectRandomColourAsTarget(colours = COLOURS) {
+  return colours[generateRandomArrayIndex()];
+}
+
+function pushUniqueItemIntoArray(array, colours = COLOURS) {
+  const randomIndex = generateRandomArrayIndex();
+  // if array already includes the colour at randomIndex, recall the function hence generating a new randomIndex.
+  array.includes(colours[randomIndex])
+    ? pushUniqueItemIntoArray(array)
+    : array.push(colours[randomIndex]);
+}
+
+function generateRandomArrayOf5Colours() {
+  const randomArrayOf5Colours = [];
+  for (let i = 0; i < 5; i++) {
+    pushUniqueItemIntoArray(randomArrayOf5Colours);
+  }
+  return randomArrayOf5Colours;
+}
+
+function shuffleAll6Colours(targetColor) {
+  const arrayOf6Colours = [...generateRandomArrayOf5Colours(), targetColor];
+  //array must be shuffled otherwise the target colour will always be at the same position.
+
+  // use array.sort method to shuffle the array based on the result of the computation which either gives a negative -1 or 0. Each result will affect the position of each item.
+  arrayOf6Colours.sort((a, b) => Math.round(Math.random()) - 1);
+  return arrayOf6Colours;
+}
